1eb66581abe14f2128f2d0d700d7d5abc8794a30
旭泽 <108265458+hxz2018877579@users.noreply.github.com>
2025-10-26 21:07:12 +0800

    V1

---
+++ web/utils/town_data.py
@@ -0,0 +1,54 @@
+# 平乐县乡镇的数据，包含名称、可点击区域的矩形坐标(x, y, width, height)和文本位置(text_x, text_y)
+# 调整了可点击的矩形区域，使其更宽敞，包含文本位置，让点击更加方便。
+TOWNS = {
+    "pinle_zhen": {
+        "name": "平乐镇",
+        "rect": (130, 270, 70, 60),
+        "text_pos": (165, 300)
+    },
+    "ertang_zhen": {
+        "name": "二塘镇",
+        "rect": (270, 220, 70, 60),
+        "text_pos": (305, 250)
+    },
+    "shazi_zhen": {
+        "name": "沙子镇",
+        "rect": (190, 110, 70, 60),
+        "text_pos": (225, 140)
+    },
+    "yuantou_zhen": {
+        "name": "源头镇",
+        "rect": (450, 450, 70, 60),
+        "text_pos": (485, 480)
+    },
+    "tongan_zhen": {
+        "name": "同安镇",
+        "rect": (370, 300, 70, 60),
+        "text_pos": (405, 330)
+    },
+    "zhangjia_zhen": {
+        "name": "张家镇",
+        "rect": (315, 280, 70, 60),
+        "text_pos": (350, 310)
+    },
+    "yangan_xiang": {
+        "name": "阳安乡",
+        "rect": (340, 400, 70, 60),
+        "text_pos": (375, 430)
+    },
+    "qiaoting_xiang": {
+        "name": "桥亭乡",
+        "rect": (250, 375, 70, 60),
+        "text_pos": (285, 405)
+    },
+    "qinglong_xiang": {
+        "name": "青龙乡",
+        "rect": (290, 390, 70, 60),
+        "text_pos": (330, 420)
+    },
+    "dafa_xiang": {
+        "name": "大发瑶族乡",
+        "rect": (130, 335, 90, 60),
+        "text_pos": (175, 365)
+    }
+}
